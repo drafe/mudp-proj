fb1d4b52a49094c8fc1d330057624bb93a4aad49
drafe <58957966+drafe@users.noreply.github.com>
2023-10-28 21:07:25 +0300

    Update documentation README.md

--- ImageChanger.py
+++ ImageChanger.py
@@ -1,7 +1,7 @@
 from PIL import Image
 from Paddinger import Style, Paddinger
 from Blurrer import Blurr, Blurrer
-from SobelEdgeDetector import Edge, SobelEdgeDetector
+from SobelEdgeDetector import SobelEdgeDetector
 
 
 
